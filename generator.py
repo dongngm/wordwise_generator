f978671974d9801149fc4a143d767306758aa841
Dong Nguyen <dong.nguyen@geniebook.com>
2023-04-14 11:01:10 +0700

    no console window when running calibre, not using tar to zip

--- generator.py
+++ generator.py
@@ -13,7 +13,11 @@ import time
 import ast
 
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s [+] %(message)s')
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s [+] %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
 logger = logging.getLogger()
 WW_SRC_COL = "word"
 WW_DES_COL = "short_def"
@@ -106,7 +110,13 @@ def ww_lookup(word_idx):
     # no change
     return (word_idx, word, 0)
 
-
+def run_subproc(cmd, capture_cmdout=True):
+    subprocess.run(
+        cmd.split(),
+        capture_output=capture_cmdout,
+        creationflags=subprocess.CREATE_NO_WINDOW,
+        check=True)
+    
 def generate(args):
     s = time.time()
     # validate args
@@ -140,14 +150,8 @@ def generate(args):
     # Convert Book to HTML
     logger.info("Converting book to HTML format")
     try:
-        subprocess.run(
-            ["ebook-convert", i_file, "book_dump.htmlz"],
-            capture_output=capture_cmdout,
-            check=True)
-        subprocess.run(
-            ["ebook-convert","book_dump.htmlz", "book_dump_html"],
-            capture_output=capture_cmdout,
-            check=True)
+        run_subproc(f"ebook-convert {i_file} book_dump.htmlz", capture_cmdout)
+        run_subproc(f"ebook-convert book_dump.htmlz book_dump_html", capture_cmdout)
     except:
         # Check if conversion is successful
         logger.error("Conversion failed. Please check if calibre is installed.")
@@ -178,17 +182,27 @@ def generate(args):
     with open("book_dump_html/index1.html", "w", encoding="utf-8") as f:
         f.write(ww_book_content)
     #
-    subprocess.run(["tar.exe", "-a",  "-c",  "-f", "book_dump_html.zip",  "book_dump_html"])
+    # subprocess.run(["tar.exe", "-a",  "-c",  "-f", "book_dump_html.zip",  "book_dump_html"])
+    shutil.make_archive("book_dump_html", "zip", "book_dump_html")
     # 
     if args.out_epub:
         logger.info("Creating word-wise EPUB...")
-        subprocess.run(["ebook-convert",  "book_dump_html.zip", f"{bookfilename}-wordwised-lvl{hint_level}.epub"], capture_output=True)
+        run_subproc(
+            f"ebook-convert book_dump_html.zip {bookfilename}-wordwised-lvl{hint_level}.epub",
+            capture_cmdout
+        )
     if args.out_azw3:
         logger.info("Creating word-wise AZW3...")
-        subprocess.run(["ebook-convert",  "book_dump_html.zip", f"{bookfilename}-wordwised-lvl{hint_level}.azw3"], capture_output=True)
+        run_subproc(
+            f"ebook-convert book_dump_html.zip {bookfilename}-wordwised-lvl{hint_level}.azw3",
+            capture_cmdout
+        )
     if args.out_pdf:
         logger.info("Creating word-wise PDF...")
-        subprocess.run(["ebook-convert",  "book_dump_html.zip", f"{bookfilename}-wordwised-lvl{hint_level}.pdf"], capture_output=True)
+        run_subproc(
+            f"ebook-convert book_dump_html.zip {bookfilename}-wordwised-lvl{hint_level}.pdf",
+            capture_cmdout
+        )
     
     # clean temp
     for p in ["book_dump.htmlz", "book_dump_html", "book_dump_html.zip"]:
@@ -197,5 +211,5 @@ def generate(args):
         if os.path.isdir(p):
             shutil.rmtree(p, ignore_errors=True)
     #
-    logger.info("Finished in {} (s). Have fun reading books.".format(time.time() - s))
+    logger.info("Finished in {:.2f} (s). Have fun reading books.".format(time.time() - s))
     logger.info("Credit: https://www.facebook.com/doduc.dnad")
